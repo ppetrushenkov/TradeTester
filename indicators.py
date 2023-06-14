3554d7601b0d21326ddd32772d859128698efb8f
ppetrushenkov <statixx_err99@yahoo.com>
2023-06-14 17:10:54 +0300

    move out candlestick patterns

--- indicators.py
+++ indicators.py
@@ -40,38 +40,6 @@ def chande_kroll_stop(high: pd.Series, low: pd.Series, close: pd.Series, p, x, q
     return pd.concat([upper, lower], keys=['up_kroll', 'dn_kroll'], axis=1)
 
 
-def impulse_candle(op, hi, lo, cl, period: int = 21, n_split: int = 4):
-    """
-    Impulse candle is the candle, that have their range greater than average true range and closes around its MAX/MIN values
-    """
-    atr = ta.ATR(hi, lo, cl, period)
-    candle_range = hi - lo
-    return np.where((candle_range > atr) & (cl > op) & (cl > hi - candle_range / n_split),  1,
-           np.where((candle_range > atr) & (cl < op) & (cl < lo + candle_range / n_split), -1, 
-                                                                                            0))
-
-
-def pinbar(op, hi, lo, cl):
-    bar_range = hi - lo
-    chunk = bar_range / 3
-    if cl > hi - chunk and op > hi - chunk:
-        return 1
-    elif cl < lo + chunk and op < lo + chunk:
-        return -1
-    else:
-        return 0
-
-
-def kangaroo(op, hi, lo, cl, period: int = 21):
-    bar_range = hi - lo
-    atr = ta.ATR(hi, lo, cl, period)
-    if bar_range > atr:
-        body = abs(op - cl)
-        pb = pinbar(op, hi, lo, cl)
-        pass
-    # TODO: continue kangaroo!
-
-
 def level_crossover(current: pd.Series, level: int = 50) -> np.array:
     """
     :returns True if prev value was under specified level and current value is over
@@ -112,7 +80,12 @@ def drawdown(profit: pd.Series):
     rolling_max = profit.cummax()
     drawdown = profit / rolling_max - 1.0
     max_drawdown = drawdown.cummin()
-    return drawdown, max_drawdown
+    return drawdown * 100, max_drawdown * 100
 
 def sharp_ratio(profit: pd.Series) -> pd.Series:
     return profit.mean() / profit.std()
+
+def linreg_trend(ma, angle: int = 30):
+    ma = ma * 1e4
+    return np.where(ma > angle, 1,
+                    np.where(ma < angle, -1, 0))
