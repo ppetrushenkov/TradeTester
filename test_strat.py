5dba65c161c4ade27d74a6f982a5767bb08ddbd2
ppetrushenkov <statixx_err99@yahoo.com>
2023-06-15 16:37:52 +0300

    remove yf

--- test_strat.py
+++ test_strat.py
@@ -3,17 +3,19 @@ from indicators import level_crosses
 import pandas as pd
 import numpy as np
 import talib as ta
-import yfinance as yf
+from indicators import donchian_channel, channel_trend
 
 
 df = pd.read_csv('eurusd_h1.csv', parse_dates=['dt'])
 df.drop('pair', axis=1, inplace=True)
 
 PERIOD = 42
-df['ma'] = ta.SMA(df['close'], PERIOD)
-df['trend'] = np.where(df['ma'] > df['ma'].shift(1),  1,
-              np.where(df['ma'] < df['ma'].shift(1), -1,
-                                                      0))
+# df['ma'] = ta.SMA(df['close'], PERIOD)
+# df['trend'] = np.where(df['ma'] > df['ma'].shift(1),  1,
+#               np.where(df['ma'] < df['ma'].shift(1), -1,
+#                                                       0))
+upper, middle, lower = donchian_channel(df['high'], df['low'], PERIOD)
+trend = channel_trend(df['high'], df['low'], upper, middle, lower)
 
 df['mfi'] = ta.MFI(df['high'], df['low'], df['close'], df['volume'], PERIOD)
 df['entries'] = level_crosses(df['mfi'], 50)
@@ -28,10 +30,13 @@ tester.add_data(
     close_data=df['close'].values
 )
 
-tester.add_trend(df['trend'])
+tester.add_trend(trend)
 tester.add_entries(df['entries'])
 tester.set_stoploss_method('channel', PERIOD)
-tester.set_takeprofit_method('atr', PERIOD, 3)
-tester.run_strategy(n=42)
+# tester.set_stoploss_method('atr', PERIOD, 1)
+tester.set_takeprofit_method('atr', PERIOD, 4)
+# tester.set_takeprofit_method('SL ratio', 5)
+# tester.set_takeprofit_method('channel', PERIOD*4, 4)
+tester.run_strategy(n=PERIOD*2)
 print(tester.form_order_statistic())
 tester.show_orders_statistic()
