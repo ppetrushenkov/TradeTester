00e56d3453dde52a74228fa4cd9bd7685cf6637f
ppetrushenkov <statixx_err99@yahoo.com>
2023-08-04 12:34:44 +0300

    move `extremum_update` to the calculate_statistic

--- calculate_statistic.py
+++ calculate_statistic.py
@@ -1,4 +1,6 @@
 from tqdm import tqdm
+from typing import Literal
+from indicators import donchian_channel
 import pandas as pd
 
 
@@ -42,3 +44,35 @@ def get_update_count(data: pd.DataFrame, candles2use: pd.Series):
                     break
 
     return update_count
+
+
+def extremum_update(data: pd.DataFrame, period: int = 21, method: Literal['channel', 'bands'] = 'bands'):
+    """
+    Return 1, if the price updates the high border in PERIOD time
+    -1, if the price updates the lower border
+    and 0 if nothing was updated
+    :param data: OHLC data. Columns must be named as 'open', 'high', 'low', 'close'
+    :param period: the number of bars, that will be used
+    :param method: Channel uses Donchian channel, bands uses Bollinger Bands method
+    :return: pd.Series | np.array
+    """
+    if method == 'channel':
+        up, low, mid = donchian_channel(data['high'], data['low'], period)
+    elif method == 'bands':
+        up, mid, low = ta.BBANDS(data['close'], period)
+    else:
+        raise ValueError("Choose either 'channel' or 'bands'")
+    output = []
+
+    for i in range(data.shape[0]):
+        chunk_data = data.iloc[i:i + period, :]
+        chunk_up = up[i]
+        chunk_low = low[i]
+
+        if any(chunk_data.high > chunk_up):
+            output.append(1)
+        elif any(chunk_data.low < chunk_low):
+            output.append(-1)
+        else:
+            output.append(0)
+    return output
--- utils.py
+++ utils.py
@@ -71,38 +71,6 @@ def expand_for_n_candles(df, num_shifts):
     return joined_df
 
 
-def extremum_update(data: pd.DataFrame, period: int = 21, method: Literal['channel', 'bands'] = 'bands'):
-    """
-    Return 1, if the price updates the high border in PERIOD time
-    -1, if the price updates the lower border
-    and 0 if nothing was updated
-    :param data: OHLC data. Columns must be named as 'open', 'high', 'low', 'close'
-    :param period: the number of bars, that will be used
-    :param method: Channel uses Donchian channel, bands uses Bollinger Bands method
-    :return: pd.Series | np.array
-    """
-    if method == 'channel':
-        up, low, mid = donchian_channel(data['high'], data['low'], period)
-    elif method == 'bands':
-        up, mid, low = ta.BBANDS(data['close'], period)
-    else:
-        raise ValueError("Choose either 'channel' or 'bands'")
-    output = []
-
-    for i in range(data.shape[0]):
-        chunk_data = data.iloc[i:i + period, :]
-        chunk_up = up[i]
-        chunk_low = low[i]
-
-        if any(chunk_data.high > chunk_up):
-            output.append(1)
-        elif any(chunk_data.low < chunk_low):
-            output.append(-1)
-        else:
-            output.append(0)
-    return output
-
-
 def create_filter(data: pd.DataFrame, query: str):
     """
     Create filter, that corresponds to the condition in query 
